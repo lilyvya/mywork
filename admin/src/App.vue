d1ad9b6eb2c9f7cbb4beff33aac18220558139df
lily <143595517+lilyvya@users.noreply.github.com>
2023-09-12 09:33:35 +0800

    样式修改

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -8,4 +8,28 @@
     margin: 0;
     padding: 0;
   }
+  box{
+    .payment-popup-content {
+  margin: 20px 0;
+}
+
+.payment-details {
+  margin: 20px 0 0 60px;
+}
+
+.payment-info {
+  display: flex;
+}
+.info-label {
+  color: #99a9bf;
+}
+.info-value {
+  color: #606266;
+  margin-left: 10px;
+}
+.dialog-footer {
+  text-align: center;
+  margin-top: 20px;
+}
+  }
 </style>
